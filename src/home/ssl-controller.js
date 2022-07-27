b70a9d635942a54aa4205a1e1b57750593bc4f13
Lourran Olimpio Zorzi <achimid@hotmail.com>
2022-07-27 14:59:30 -0300

    tentantiva de challeng

--- src/home/ssl-controller.js
+++ src/home/ssl-controller.js
@@ -4,4 +4,8 @@ router.get('/.well-known/acme-challenge/7Uc3Y3mJms3xCB0J0EV7Yi1ce-U0Ls4CUpj0zXBG
     res.send("7Uc3Y3mJms3xCB0J0EV7Yi1ce-U0Ls4CUpj0zXBGvX8.uKBuafrqRprc3yyxfUTmtDzw1YWdyLN_NnzUj5u_5UA")
 })
 
+router.get('/.well-known/acme-challenge/ASfR46OKiv8Dr8l-tSfYCxVRPej7utbTH3xaMKXSEA4', (req, res) => {
+    res.send("ASfR46OKiv8Dr8l-tSfYCxVRPej7utbTH3xaMKXSEA4.uKBuafrqRprc3yyxfUTmtDzw1YWdyLN_NnzUj5u_5UA")
+})
+
 module.exports = router
